49fe80b7f52ebe217c7b8925c5c95f3ed79dd3c9
MOV <osmar@foxmail.com>
2023-06-15 00:27:16 +0800

    Update .dumirc.ts

--- .dumirc.ts
+++ .dumirc.ts
@@ -1,7 +1,14 @@
 import { defineConfig } from 'dumi';
 
+const repo = 'doc'; // 项目名
+
 export default defineConfig({
-  themeConfig: {
-    name: 'doc-test',
+  title: 'Site Name',
+  mode: 'site',
+  devServer: {
+    port: 5001 // 自定义端口号
   },
+  base: process.env.NODE_ENV === 'production' ? `/${repo}/` : '/',
+  publicPath: process.env.NODE_ENV === 'production' ? `/${repo}/` : '/',
+  // more config: https://d.umijs.org/config
 });
