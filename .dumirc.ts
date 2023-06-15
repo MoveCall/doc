35fb4afb6e3a2a4e90c6565218d28e8e262951fe
osmar <osmar@foxmail.com>
2023-06-15 23:30:58 +0800

    fix: logo

--- .dumirc.ts
+++ .dumirc.ts
@@ -4,9 +4,10 @@ const repo = 'doc'; // 项目名
 
 export default defineConfig({
   title: 'Site Name',
+  logo: 'https://cloud-sh.oss-cn-shanghai.aliyuncs.com/picture/R-C.png',
   mode: 'site',
   devServer: {
-    port: 5001 // 自定义端口号
+    port: 5001, // 自定义端口号
   },
   base: process.env.NODE_ENV === 'production' ? `/${repo}/` : '/',
   publicPath: process.env.NODE_ENV === 'production' ? `/${repo}/` : '/',
