8311d0e04c2648f8a68dbc4034969641879d8eff
osmar <osmar@foxmail.com>
2023-06-20 23:51:27 +0800

    add:navs

--- .umirc.ts
+++ .umirc.ts
@@ -11,4 +11,22 @@ export default defineConfig({
   base: process.env.NODE_ENV === 'production' ? `/${repo}/` : '/',
   publicPath: process.env.NODE_ENV === 'production' ? `/${repo}/` : '/',
   // more config: https://d.umijs.org/config
+
+  // 单语言配置方式如下
+  navs: [
+    null, // null 值代表保留约定式生成的导航，只做增量配置
+    {
+      title: 'GitHub',
+      path: 'https://github.com/umijs/dumi',
+    },
+    {
+      title: '我有二级导航',
+      path: '链接是可选的',
+      // 可通过如下形式嵌套二级导航菜单，目前暂不支持更多层级嵌套：
+      children: [
+        { title: '第一项', path: 'https://d.umijs.org' },
+        { title: '第二项', path: '/guide' },
+      ],
+    },
+  ],
 });
